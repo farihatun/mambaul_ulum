fedb06b60d365eebec21c0456091a9d0095948ea
Farihatun Aini <ainifarihatun2@gmail.com>
2026-06-23 22:26:01 +0800

    update add module to api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AbsensiController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\ModulController;
 
 
 /*
