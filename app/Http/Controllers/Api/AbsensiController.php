26a90254fd105dd3d039ff69e50ea9dc7d523f88
Farihatun Aini <ainifarihatun2@gmail.com>
2026-06-25 00:31:55 +0800

    update module edit

--- app/Http/Controllers/Api/AbsensiController.php
+++ app/Http/Controllers/Api/AbsensiController.php
@@ -32,7 +32,7 @@ class AbsensiController extends Controller
         ]);
 
         $cek = Absensi::where('user_id', $request->user_id)
-            ->where('tanggal', '==', $today)
+            ->whereDate('tanggal', '=', $today)
             ->first();
 
         if ($cek) {
