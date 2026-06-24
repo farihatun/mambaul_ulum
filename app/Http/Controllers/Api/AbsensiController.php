f4f5157882d79494c00d382a1306b6a067f45194
Farihatun Aini <ainifarihatun2@gmail.com>
2026-06-25 00:18:41 +0800

    update module edit

--- app/Http/Controllers/Api/AbsensiController.php
+++ app/Http/Controllers/Api/AbsensiController.php
@@ -25,7 +25,7 @@ class AbsensiController extends Controller
     {
         $today = Carbon::today()->toDateString();
         $request->validate([
-            'user_id' => 'required|exists:users_id',
+            'user_id' => 'required|exists:users,id',
             'tanggal' => 'required|date',
             'status' => 'required|in:Hadir,Izin,Sakit,Alpa',
             'alasan' => 'nullable|string'
